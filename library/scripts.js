4f5a169590007e56bba1179e91c2c6b8a562dd3d
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-09-11 01:01:06 +0300

    add sorry section

--- library/scripts.js
+++ library/scripts.js
@@ -24,3 +24,31 @@ function f1(e) {
 
 
 console.log("Никак не сделаю сецию LibraryCard, в About не хватает стрелок")
+
+// //burger
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("menu-burger-button").addEventListener("click", function() {
+//         document.querySelector(".header-links").classList.toggle("open")
+//     })
+// });
+
+// //Close burger by Esc
+// window.addEventListener('keydown', (e) => {
+//     if (e.key === "Escape") {
+//         document.querySelector(".header-links").classList.remove("open")
+//     }
+// });
+
+// // Close burger by outer click
+// document.getElementById("open-menu-burger").addEventListener('click', event => {
+//     /*event._isClickWithInMenu = true; menu is still open*/
+//     if (event._isClickWithInMenu) return;
+//     document.querySelector(".header-links").classList.remove("open")
+// });
+// document.getElementById("menu-burger-button").addEventListener('click', event => {
+//     event._isClickWithInMenu = true;
+// });
+// document.body.addEventListener('click', event => {
+//     if (event._isClickWithInMenu) return;
+//     document.querySelector(".header-links").classList.remove("open")
+// });
