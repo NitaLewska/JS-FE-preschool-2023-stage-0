6563680c108fc31a31d74f9f5e3917f3602c2074
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-10-02 01:38:29 +0300

    feat: link the script to the button

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 async function refreshPage() {
-    let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=cats&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`);
+    let response
+    if (document.querySelector('.keyword').value.length > 1) {
+        response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${document.querySelector('.keyword').value}&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`);
+    }
     let responseJSON = await response.json();
     let imgs = responseJSON.results;
     console.log(imgs)
+    document.querySelector('.pics_container').innerHTML=''
     imgs.map(function (a) {
         let image = document.createElement('img');
         image.src = a.urls.regular;
         document.querySelector('.pics_container').append(image)
     })
 }
-refreshPage()
+// refreshPage()
+
+
+
+document.querySelector(".submitButton").addEventListener('click', refreshPage)
+
 
