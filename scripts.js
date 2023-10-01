dfa64873d61b5902a8ac58ba20b233b5d333890d
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-10-02 02:22:59 +0300

    feat: add the ability to use Enter btn; add sorry text for no response; add delete text btn

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 async function refreshPage() {
     let response
-    if (document.querySelector('.keyword').value.length > 1) {
+    if (document.querySelector('.keyword').value.length >= 1) {
         response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${document.querySelector('.keyword').value}&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`);
     } else {
         response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=popular&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`)
@@ -8,17 +8,37 @@ async function refreshPage() {
     let responseJSON = await response.json();
     let imgs = responseJSON.results;
     console.log(imgs)
-    document.querySelector('.pics_container').innerHTML=''
-    imgs.map(function (a) {
-        let image = document.createElement('img');
-        image.src = a.urls.regular;
-        document.querySelector('.pics_container').append(image)
-    })
+    document.querySelector('.pics_container').innerHTML = ''
+    if (imgs.length > 0) {
+        imgs.map(function (a) {
+            let image = document.createElement('img');
+            image.src = a.urls.regular;
+            document.querySelector('.pics_container').append(image)
+        })
+    }
+    else {
+        let sorryText = document.createElement('h2');
+        sorryText.innerHTML = "There's no such photos on Unsplash, sorry. Try again!"
+        document.querySelector('.pics_container').append(sorryText)
+    }
+
 }
 refreshPage()
 
-
-
 document.querySelector(".submitButton").addEventListener('click', refreshPage)
+document.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        refreshPage()
+    }
+});
+
+document.querySelector('.keyword').addEventListener('input', function () {
+    document.querySelector('.keyword').value.length > 0 ? document.querySelector(".clearButton").classList.remove('hidden') : document.querySelector(".clearButton").classList.add('hidden')
+})
+
+document.querySelector(".clearButton").addEventListener('click', function () {
+    document.querySelector('.keyword').value = ''
+})
 
 
