5654b009312d620041be3462d877bfdaf98d03ca
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-10-02 01:43:11 +0300

    feat: update page on load

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ async function refreshPage() {
     let response
     if (document.querySelector('.keyword').value.length > 1) {
         response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${document.querySelector('.keyword').value}&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`);
+    } else {
+        response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=popular&client_id=qKm_uGWhkmgjkXjYxGXDnrEOawZNVk79XluAOAtJYG8&per_page=20`)
     }
     let responseJSON = await response.json();
     let imgs = responseJSON.results;
@@ -13,7 +15,7 @@ async function refreshPage() {
         document.querySelector('.pics_container').append(image)
     })
 }
-// refreshPage()
+refreshPage()
 
 
 
